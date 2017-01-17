69cd3ea26fea63340fb65cd51dc43990cb749ae5
ajayapra <ajayapra@mtu.edu>
2017-01-17 11:00:51 -0500

    Adding ROS tutorial files

---
+++ ajayapra/cp1_wp/sandbox/beginner_tut/build/gtest/CMakeFiles/gtest_main.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/gtest_main.dir/src/gtest_main.cc.o"
+  "/home/aj/git_work/legendary-broccoli/ajayapra/cp1_wp/sandbox/beginner_tut/lib/libgtest_main.pdb"
+  "/home/aj/git_work/legendary-broccoli/ajayapra/cp1_wp/sandbox/beginner_tut/lib/libgtest_main.so"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/gtest_main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
